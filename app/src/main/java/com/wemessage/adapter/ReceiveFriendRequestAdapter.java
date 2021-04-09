33ecda26377cf0c1a4520fb22beb47bb418ccf57
sang <sangdang991114@gmail.cim>
2021-04-09 07:58:23 +0700

    abc
    Signed-off-by: sang <sangdang991114@gmail.cim>

---
+++ app/src/main/java/com/wemessage/adapter/ReceiveFriendRequestAdapter.java
@@ -0,0 +1,103 @@
+package com.wemessage.adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.wemessage.R;
+import com.wemessage.fragment.ReceiveFriendRequestFragment;
+import com.wemessage.model.FriendInfo;
+
+import java.util.ArrayList;
+
+public class ReceiveFriendRequestAdapter extends RecyclerView.Adapter<ReceiveFriendRequestAdapter.Holder> {
+    ArrayList<String> list;
+    Activity context;
+    DatabaseReference myRequestRef,userRef;
+    ReceiveFriendRequestFragment requestFragment;
+
+    String currentUserId;
+
+
+
+
+    public ReceiveFriendRequestAdapter(ArrayList<String> list, Activity context, ReceiveFriendRequestFragment requestFragment, String currentUserId) {
+        this.list=list;
+        this.context = context;
+        this.requestFragment = requestFragment;
+        currentUserId = currentUserId;
+        myRequestRef = FirebaseDatabase.getInstance().getReference().child("FriendRequests").child(currentUserId);
+        userRef = FirebaseDatabase.getInstance().getReference().child("Users");
+    }
+
+
+    @NonNull
+    @Override
+    public ReceiveFriendRequestAdapter.Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View view = inflater.inflate(R.layout.item_friend_request_list_layout_2, parent,false);
+
+        return (new Holder(view));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ReceiveFriendRequestAdapter.Holder holder, int position) {
+        String friendID = list.get(position);
+        userRef.child(friendID).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                holder.tvName.setText(snapshot.child("name").getValue().toString());
+                if (snapshot.hasChild("avatar"))
+                {
+                    Glide.with(context).load(snapshot.child("avatar").getValue().toString()).into(holder.ivAvatar);
+                }
+                if (snapshot.hasChild("status")){
+                    holder.tvInfo.setText(snapshot.child("status").getValue().toString());
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class Holder extends RecyclerView.ViewHolder {
+        ImageView ivAvatar,ivCancel;
+        TextView tvName, tvInfo;
+        Button btnRequest;
+        RelativeLayout layout;
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+            ivAvatar = itemView.findViewById(R.id.ivAvatar);
+            ivCancel = itemView.findViewById(R.id.ivCancel);
+            tvName = itemView.findViewById(R.id.tvName);
+            tvInfo = itemView.findViewById(R.id.tvInfo);
+            btnRequest = itemView.findViewById(R.id.btnRequest);
+            layout = itemView.findViewById(R.id.layout);
+        }
+    }
+}
---
+++ app/src/main/java/com/wemessage/fragment/ReceiveFriendRequestFragment.java
@@ -0,0 +1,83 @@
+package com.wemessage.fragment;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.wemessage.R;
+import com.wemessage.adapter.ReceiveFriendRequestAdapter;
+
+import java.util.ArrayList;
+
+public class ReceiveFriendRequestFragment extends Fragment {
+    RecyclerView rcv;
+    FirebaseAuth mAuth;
+    FirebaseUser currentUser;
+    String currentUserId;
+    DatabaseReference usersRef, requestRef;
+    ReceiveFriendRequestAdapter adapter;
+    ArrayList<String>list;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_reveived_friend_request, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        rcv = getView().findViewById(R.id.rcv);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getContext());
+        rcv.setLayoutManager(layoutManager);
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        currentUserId = currentUser.getUid();
+        usersRef = FirebaseDatabase.getInstance().getReference().child("Users");
+        requestRef = FirebaseDatabase.getInstance().getReference().child("FriendRequests");
+        list= new ArrayList<>();
+
+        displayRcv();
+
+    }
+
+    public void displayRcv(){
+        requestRef.child(currentUserId).orderByChild("type").equalTo("receive").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list.clear();
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    list.add(dataSnapshot.getKey());
+
+                }
+                Log.d("Loi", "onDataChange: size " + list.size());
+                adapter=new ReceiveFriendRequestAdapter(list, getActivity(),ReceiveFriendRequestFragment.this,currentUserId);
+                rcv.setAdapter(adapter);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+}
