1a531b0c4a87e3f1e92ff024fdf5c289304693a6
Ngô Quốc Vinh <71309432+ngongo12@users.noreply.github.com>
2021-04-10 13:41:05 +0700

    test

--- app/src/main/java/com/wemessage/adapter/SuggestionAdapter.java
+++ app/src/main/java/com/wemessage/adapter/SuggestionAdapter.java
@@ -58,8 +58,14 @@ public class SuggestionAdapter extends RecyclerView.Adapter<SuggestionAdapter.Ho
 
     @Override
     public void onBindViewHolder(@NonNull Holder holder, int position) {
-        holder.tvName.setText(list.get(position).getName());
-        holder.tvInfo.setText(list.get(position).getStatus());
+
+        if (list.get(position).getName() != null)
+        {
+            holder.tvName.setText(list.get(position).getName());
+        }
+        if(list.get(position).getStatus() != null) {
+            holder.tvInfo.setText(list.get(position).getStatus());
+        }
 
         //Nếu có avatar thì hiển thị không thì mặc định
         if (list.get(position).getAvatar() != null)
